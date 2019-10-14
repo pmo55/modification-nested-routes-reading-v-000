ad0f0fa0d9fbbef6a89d2a312c7c23779d0b9b87
Parsa Moshiri <parsamoshiri5@gmail.com>
2019-10-14 17:30:35 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
   def author_id_field(post)
     if post.author.nil?
-      select_tag "post[author_id]", options_from_collection_for_select(Author.all,:id, :name )
+      select_tag "post[author_id]", options_from_collection_for_select(Author.all,:id )
     else hidden_field_tag "post[author_id]", post.author_id
    end
   end
